750626e39b5fe3a799521fe9281dbb57cf715684
Kaiser <amanda.8508278@aluno.mg.gov.br>
2026-08-31 10:15:33 -0300

    feat: validar seleção da turma

--- SistemaVotacao.java
+++ SistemaVotacao.java
@@ -159,6 +159,20 @@ public class SistemaVotacao {
             );
         }
     }
+    static int obterIndiceTurma() {
+        int turma;
+
+        do {
+            turma = lerInteiro("Informe a turma de 1 a 3: ");
+
+            if (turma < 1 || turma > TOTAL_TURMAS) {
+                System.out.println("Turma inválida.");
+            }
+        } while (turma < 1 || turma > TOTAL_TURMAS);
+
+        return turma - 1;
+    }
+
 }
 
 
